e1364a02ba80046ca9a027321e3c4412228aee4e
jessie <104639236@qq.com>
2017-04-26 01:12:29 +0800

    初始化scrapy项目

---
+++ qiubai/qiubai/spiders/indexpage.py
@@ -0,0 +1,6 @@
+import scrapy
+class QiuBaiSpider(scrapy.Spider):
+    name = "qiubai"
+    start_urls=["http://www.qiushibaike.com/",]
+    def parse(self, response):
+        print response.xpath('//div[@class="content"]').extract()
